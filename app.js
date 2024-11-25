6c599ca0c2cf809a4db549357126abfd503d0116
Kevin Aguaisa <154232211+kaaguaisa69@users.noreply.github.com>
2024-11-25 00:02:35 -0500

    firts version server Node.js

---
+++ app.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+const PORT = 3000;
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Servidor funcionando correctamente ');
+});
+
+server.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
