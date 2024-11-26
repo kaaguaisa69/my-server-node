66aadf3b772d6cc8052dd7ed3940fc2fa794a2d1
Kevin Aguaisa <154232211+kaaguaisa69@users.noreply.github.com>
2024-11-26 18:25:04 -0500

    feat: :zap: public ip configuration

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const http = require('http');
+const express = require('express');
+const path = require('path');
 
+const app = express();
 const PORT = 3000;
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Servidor funcionando correctamente, pruba 2 ');
+
+// Configurar motor de vistas
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views')); // Carpeta para las vistas
+
+// Ruta principal para renderizar la vista index.ejs
+app.get('/', (req, res) => {
+    res.render('index'); // Renderiza la vista index.ejs
 });
 
+// Crear servidor HTTP
+const server = http.createServer(app);
+
+// Iniciar el servidor
 server.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
