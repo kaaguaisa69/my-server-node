b5e1817f79007e604df122a09a587b0e4e02a853
Kevin Aguaisa <154232211+kaaguaisa69@users.noreply.github.com>
2024-11-26 13:40:29 -0500

    feat: :art: Update text

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const http = require('http');
 const PORT = 3000;
 const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Servidor funcionando correctamente ');
+    res.end('Servidor funcionando correctamente, pruba 2 ');
 });
 
 server.listen(PORT, () => {
